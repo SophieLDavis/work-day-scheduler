ed4c1685ac71968c44d4021d005bb75889520f7b
Sophie <sophielouisedavis@live.com>
2023-02-06 17:40:05 +0000

    first user input set to local storage

--- script.js
+++ script.js
@@ -1,74 +1,90 @@
-//2. Each timeblock need to have the time, textarea, and save button -->
-//$('.container').append()
-//equivalent to event.target.getAttribute("data-hour")
-//console.log($( "div" ).data("hour").attr());
-//console.log(jQuery.data(document.body, 'hour'))
-var time = parseInt($('.data-hour'))
-var amPm = moment().format('A');
-timeAmPm= time + amPm
-$('.timeblock').append('<div class="hour"></div>').text(timeAmPm);// error 
-
-
-// div2 user text input area/ form input - event listener 
-$('.timeblock').append('<textarea ></textarea>').on('submit', function(event){
-    //style text area to be longer?
-    //preventDefault;
-});
-
-//$('.timeblock')
-//div 3 save button
-$('.timeblock').append('<button class="saveBtn"></button>');
-//add icon ? <i class="fa-solid fa-floppy-disk"></i>
-
-
 // Show the current date at the top
-$('#currentDay').text(moment().format('HH MMMM Y HH:mm'));
+$('#currentDay').text(moment().format('dddd DD MMMM Y HH:mm'));
+
+//add icon 
+//$('.saveBtn').append('<i class="fa fa-lock"></i>');
 
 // Colour code each block based on the current time
 // Create variables to target each time block
-
 //Create variable for moment().format('H') (0 - 23) e.g. thisHour
 var thisHour= moment().format('H');
 var timeblocks = $('.timeblock');
-//what does .each do?   iterates over the element and returns it when this is used. 
-
+//var hour = $(this).attr('data-hour');
 timeblocks.each(function() {
 //selects 'data-hour' as whatever is set to hour
 var hour = $(this).attr('data-hour');
 //`hour` and `thisHour` should be an integer:
+//console.log("hour", hour)
 hour = parseInt(hour, 10)
+console.log("hour", hour)
 thisHour = parseInt(thisHour, 10)
-
+console.log("thisHour", thisHour)
 //set colours for time
 var past= $('.past')
 var present=$('.present')
 var future= $('.future')
 
+// if (hour < thisHour) {
+//     $('textarea').css('background-color', past); 
+//     } else if (hour == thisHour) {
+//     $('textarea').css('background-color', present);
+//     } else {
+//     $('textarea').css('background-color', future);
+//     }
+//     })
+
 if (hour < thisHour) {
-$(this).css('background-color', past)
-} else if (hour == thisHour) {
-$(this).css('background-color', present)
-} else {
-$(this).css('background-color', future)
-}
+$('textarea').addClass('past').removeClass('present').removeClass('future'); 
+ } else if (hour === thisHour) {
+// $('textarea').addClass('present').removeClass('past').removeClass('future');
+// } else {
+// $('textarea').addClass('future').removeClass('past').removeClass('present');
+// }
+}})
+
+// 3. Save input to local storage
+
+//create variable (an array) called inputs that will store all of the input data
+
+var inputs=[];
+//Add event listener to all save button
+$('.saveBtn').on('click', function(event){
+event.preventDefault;
+console.log($('textarea').val()) // only 9am works 
+//Push the input value to inputs array with the format of { time: xx, input: xxx }
+//inputs.push({time: xx, input: xx})
+var inputVal= $('textarea').val()
+inputs.push({"time:" :  hour, "input:" : inputVal})  
+
+//Show feedback message to the user (optional)
+//The feedback need to be dissappeared automatically
+$('.jumbotron').append('<p class="alert">Your information has been saved.</p>')
+//$('.alert').text('')
+
+//Save inputs variable to local storage + stringify the inputs array
+(localStorage.setItem('inputs', JSON.stringify(inputs)))
 })
 
 
-// 3. Save input to local storage
-//      create variable (an array) called inputs that will store all of the input data
-//      Add event listener to all save button
-//          Add event.preventDefault inside the click event listener
-//          Push the input value to inputs array with the format of { time: xx, input: xxx }
-//              Get the input value
-//              Get the hour value
-//              Push to the inputs array if the hour entry not exist yet in the array
-//              Replace the existing entry if the hour entry exist in the array
-//          Save inputs variable to local storage
-//              Stringify the inputs array
-//          Show feedback message to the user (optional)
-//          The feedback need to be dissappeared automatically
-//
-//      Example of inputs array format
+//Get the hour value  $('.data-hour')
+
+//Push to the inputs array if the hour entry not exist yet in the array
+//Replace the existing entry if the hour entry exist in the array
+//look up check object in array check if hour = this .datahour
+
+//if (hour doesnt exist in inputs array){
+// inputs.push(input and hour)
+//} else if (hour does exist){
+//inputs.replace(input and hour)
+//}
+
+// if (hour!== inputs) {
+//     inputs.push({"time:" :  thisHour, "input:" : inputVal})
+// } else if (hour== inputs) {
+// replaceWith()
+// }
+
+//Example of inputs array format
 //      var inputs = [{
 //        time: 9,
 //        input: 'Meeting',
@@ -76,19 +92,21 @@ $(this).css('background-color', future)
 //        time: 10,
 //        input: 'Coffee',
 //      },...];
+
 // 4. Load input from local storage when page load/refresh if there's any data in local storage
-//      var localStorageInput = get data from local storage
-//      check if localStorageInput exist, if it is
-//          var parsedLocalStorageInput = parse localStorageInput
-//          populate the inputs with the value from parsedLocalStorageInput
-//          inputs.forEach(function(input) {
-//              input => {
-//                time: 9,
-//                input: 'Meeting',
-//              }
-//              $('.timeblock[data-hour="' + input.time + '"] textarea').val(input.input)
-//          })
-//      if theres no data in local storage, do nothing
 
-// Extra, add hover effect on the save button
+var savedInputs= localStorage.getItem('inputs')
+var parsedInputs= JSON.parse(savedInputs)
 
+
+//check if savedInput/parsedInputs exist, if it does then.. how to check if it exists? if .length =<1
+
+// populate the inputs with the value from parsedInputs
+
+// inputs.forEach(function(input) {
+
+//  $('.timeblock [data-hour=" ' + inputs.time + '"] textarea').val(inputs.input)
+// // // data-hour= 'input.time'
+// // // $('textarea').val(parsedInputs)
+// })
+// if theres no data in local storage, do nothing 
